a152f245dfddc57fc2cb70586b567f9cd19124ff
David <dhorram948@g.educaand.es>
2024-02-13 19:08:53 +0100

    error de compilacion

--- src/main/java/com/cpifp/alanturing/holamundo/domain/Hello.java
+++ src/main/java/com/cpifp/alanturing/holamundo/domain/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
     private String greeting;
 
     public Hello(String greeting) {
-        this.greeting = greeting;
+        this.greeting = greeting
     }
 
     public String getGreeting() {
